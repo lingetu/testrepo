a08cdb6c992af32dab2938cc02ffa72f5ddc9c8b
LingEtu <lucas.bondon@etu.univ-amu.fr>
2024-10-07 09:43:02 +0200

    fin

--- pipelineCiCd.py
+++ pipelineCiCd.py
@@ -1,19 +1,25 @@
+"""
+This module contains unit tests for the greater_than function.
+"""
+
 import unittest
 from test import greater_than
 
 class TestGreaterThan(unittest.TestCase):
+    """Unit tests for the greater_than function."""
 
-   def setUp(self):
+    def setUp(self):
         self.value_a = 15
         self.value_b = 20
 
-   def test_not_greater(self):
-        """Test de a > b = False"""
+    def test_not_greater(self):
+        """Test that a > b returns False."""
         self.assertFalse(greater_than(self.value_a, self.value_b))
 
-   def test_greater(self):
-        """Test de b > a = True"""
+    def test_greater(self):
+        """Test that b > a returns True."""
         self.assertTrue(greater_than(self.value_b, self.value_a))
 
 if __name__ == '__main__':
     unittest.main()
+
--- test.py
+++ test.py
@@ -1,2 +1,3 @@
 def greater_than(a, b):
+   """Return True if a is greater than b, False otherwise."""
    return a > b
